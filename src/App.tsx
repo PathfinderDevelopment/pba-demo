d159092a0efcd43241ea409a6b8b5b99b2851238
Aaron Sutton <aaron@orthogonal.io>
2020-03-25 11:28:49 -0500

    Changed layout structure and built pairing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import {Route} from 'react-router';
 import {Home} from './Components/Home/Home';
 import {Pairing} from './Components/Pairing/Pairing';
@@ -7,17 +7,33 @@ import {Feedback} from './Components/Feedback/Feedback';
 import {CalorieInput} from './Components/CalorieInput/CalorieInput';
 import {Alerts} from './Components/Alerts/Alerts';
 import {Login} from './Components/Login/Login';
+import styled from 'styled-components';
 
-export const App:React.FC = () => {
+const InnerContainer = styled.div`
+  display:table-cell;
+  vertical-align:middle;
+`;
+
+const StyledContainer = styled.div`
+  display: table;
+  position: absolute;
+  width:100%;
+  height:100%;
+`;
+
+
+export const App: React.FC = () => {
   return (
-    <Fragment>
-      <Route exact path='/' component={Login}/>
-      <Route exact path='/pairdevice' component={Pairing}/>
-      <Route exact path='/home' component={Home}/>
-      <Route exact path='/insulindosage' component={InsulinDosage}/>
-      <Route exact path='/feedback' component={Feedback}/>
-      <Route exact path='/calorieinput' component={CalorieInput}/>
-      <Route exact path='/alerts' component={Alerts}/>
-    </Fragment>
+    <StyledContainer>
+      <InnerContainer>
+        <Route exact path='/' component={Login} />
+        <Route exact path='/pairdevice' component={Pairing} />
+        <Route exact path='/home' component={Home} />
+        <Route exact path='/insulindosage' component={InsulinDosage} />
+        <Route exact path='/feedback' component={Feedback} />
+        <Route exact path='/calorieinput' component={CalorieInput} />
+        <Route exact path='/alerts' component={Alerts} />
+      </InnerContainer>
+    </StyledContainer>
   );
 };
