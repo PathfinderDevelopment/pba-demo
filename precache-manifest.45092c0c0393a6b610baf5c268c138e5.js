214bfc7acf24fac9fd85bdebe0deca0f8bd4afa5
Aaron Sutton <aaron@orthogonal.io>
2020-04-03 11:51:44 -0500

    Updates

---
+++ precache-manifest.45092c0c0393a6b610baf5c268c138e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ac8922b7028330437e96d5ce7e399b",
+    "url": "/pba-demo/index.html"
+  },
+  {
+    "revision": "1e2a8f6ae0b92a973f92",
+    "url": "/pba-demo/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "9af46bcf60a812dbf680",
+    "url": "/pba-demo/static/css/main.24ba5be8.chunk.css"
+  },
+  {
+    "revision": "1e2a8f6ae0b92a973f92",
+    "url": "/pba-demo/static/js/2.6748474c.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/pba-demo/static/js/2.6748474c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af46bcf60a812dbf680",
+    "url": "/pba-demo/static/js/main.c71eb095.chunk.js"
+  },
+  {
+    "revision": "a6049dd68d79ed0f1942",
+    "url": "/pba-demo/static/js/runtime-main.2a3925b0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pba-demo/precache-manifest.cc8522fdc363cebe3eff5fd06598e044.js"
+  "/pba-demo/precache-manifest.45092c0c0393a6b610baf5c268c138e5.js"
 );
 
 self.addEventListener('message', (event) => {
